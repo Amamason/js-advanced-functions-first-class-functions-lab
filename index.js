4aa960712edcd6e7201e23da5aa53bd0a863aa24
Amamason <104336445+Amamason@users.noreply.github.com>
2022-06-12 12:52:26 +1000

    commit tree a5a9cf0aa1b854cced1e6d10b16cabdfbd439bda

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // Code your solution in this file!
+/*
+
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+  };
+
+  const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+  };
+
+  const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers ];
+
+  function createFareMultiplier(num){
+      return function (number){
+         return number * num
+      }
+  }
+
+//   const fareDoubler = createFareMultiplier(2)
+
+  function fareTripler(num){
+    return num * 3
+  }
+
+  function fareDoubler(num){
+    return num * 2
+  }
+
+  function fetchSpecifiedDrivers(array, func){
+    return func(array)
+  }
+
+*/
+
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+}
+
+selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num){
+    return function (number){
+       return number * num
+    }
+}
+
+function fareDoubler(num){
+    return num * 2;
+}
+
+function fareTripler(num){
+    return num * 3;
+}
+
+function selectingDrivers(drivers){
+ return (FirstTwoDrivers(), 
+     drivers.slice(-2));
+ } 
+
+ 
+
+ const selectDifferentDrivers = function (drivers, whichDrivers) {
+    return whichDrivers(drivers);
+  }; 
